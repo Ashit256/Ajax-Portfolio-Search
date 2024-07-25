102e83ff7a8d9e65e9a7d27dd68121f63338a761
Ashit256 <ashit@unlimitedwp.com>
2024-07-25 15:44:16 +0530

    Create portfolio-search.js

---
+++ portfolio-search.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+    $('#portfolio-search').on('keyup', function() {
+        var searchQuery = $(this).val();
+
+         if (searchQuery === '') {
+            $('#portfolio-results').empty(); // Clear results if search field is empty
+            return;
+        }
+
+        $.ajax({
+            url: portfolioAjax.ajaxurl,
+            type: 'post',
+            data: {
+                action: 'portfolio_ajax_search',
+                searchQuery: searchQuery
+            },
+            success: function(response) {
+                $('#portfolio-results').html(response);
+            }
+        });
+    });
+});
